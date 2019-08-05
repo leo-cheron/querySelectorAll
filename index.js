33d045a22e5cff9903e95a0e6ebd693a59a7db10
Leo Cheron <leo@cheron.works>
2019-08-05 17:11:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const queryselector = (selector, context = document) => {
+	if (typeof selector !== 'string') {
+		console.warn('Required argument selector is not a String or undefined: ', selector);
+	}
+
+    const result = context.querySelectorAll(selector);
+
+    return result;
+};
+
+if (!Node.prototype.$) {
+	window.$ = Node.prototype.$ = NodeList.prototype.$ = function(selector, context = document) {
+		return queryselector(selector, context);
+	};
+}
